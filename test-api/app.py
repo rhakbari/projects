bbe7bd9df7c77e35d207ea2515e7313e52261edc
Raza Hussain Akbari <rhakbari.96@gmail.com>
2020-01-25 23:47:43 +0500

    Update app.py

--- test-api/app.py
+++ test-api/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/currency/euro/cad/<float:euro>')
 def currency_conversion(euro):
 
-    response = requests.get("http://data.fixer.io/api/latest?access_key=eaa9582d9601a52970b9be6d5d9f89f3&symbols=USD,CAD").content
+    response = requests.get("http://data.fixer.io/api/latest?access_key=ACCESS_KEY&symbols=USD,CAD").content
     data = json.loads(response)
     rates = data["rates"]
     msg_str = "message: We have received CAD:"
