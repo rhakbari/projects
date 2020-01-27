637c7160945866ba2412a394cebab797eb7ed160
Raza Hussain Akbari <rhakbari.96@gmail.com>
2020-01-27 21:08:25 +0500

    Update app.py

--- test-api/app.py
+++ test-api/app.py
@@ -1,20 +1,23 @@
 import json
 from flask import Flask, jsonify, request
 import requests
+
 app = Flask(__name__)
 
 
-@app.route('/currency/euro/cad/<float:euro>')
+@app.route("/currency/euro/cad/<float:euro>")
 def currency_conversion(euro):
 
-    response = requests.get("http://data.fixer.io/api/latest?access_key=ACCESS_KEY&symbols=USD,CAD").content
+    response = requests.get(
+        "http://data.fixer.io/api/latest?access_key=ACCESS_KEY&symbols=USD,CAD"
+    ).content
     data = json.loads(response)
     rates = data["rates"]
     msg_str = "message: We have received CAD:"
-    cad = rates['CAD'] * euro
+    cad = rates["CAD"] * euro
     return jsonify({msg_str: cad})
+
 
 if __name__ == "__main__":
     app.run(port=5000)
-
-    
+  
